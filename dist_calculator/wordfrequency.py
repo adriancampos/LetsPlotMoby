a3691376f2c1f41bf26a65b201b7c926fd19440e
jburb <joshuabourbon@gmail.com>
2017-05-09 13:07:12 -0700

    hey guys -- this may not work at all it is dashed off untested. gonna do the actual testing/changes as needed this evening

---
+++ dist_calculator/wordfrequency.py
@@ -0,0 +1,14 @@
+from string import punctuation as punc_chars # this is a string of punctuation chars from python standard lib
+
+def get_word_frequency_dist(phrase):
+    # TODO determine proper order of stripping punctuation and stripping newlines, + correct replacement char for punc
+    phrase = phrase.replace('\r', ' ').replace('\n', ' ')
+    for char in punc_chars:
+        phrase = phrase.replace(char, ' ')
+
+    tokenized_phrase = phrase.split(' ')
+    normalized_phrase = [token.lower for token in tokenized_phrase]
+    unique_tokens = list(set(normalized_phrase))
+    word_frequency_dist = {word:unique_tokens.count(word) for word in unique_tokens}
+
+    return word_frequency_dist
