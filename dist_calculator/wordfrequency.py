0b9e8e706a1b86b74b9df307c1f18b87c93b40dc
jburb <joshuabourbon@gmail.com>
2017-05-09 19:03:58 -0700

    updating to return top 10 words only

--- dist_calculator/wordfrequency.py
+++ dist_calculator/wordfrequency.py
@@ -1,12 +1,20 @@
 from string import punctuation as punc_chars # this is a string of punctuation chars from python standard lib
+from collections import OrderedDict
 
-def get_word_frequency_dist(phrase):
-    # TODO determine proper order of stripping punctuation and stripping newlines, + correct replacement char for punc
-    phrase = phrase.replace('\r', ' ').replace('\n', ' ')
+
+def get_frequency_dist_words_top10(phrase):
+    # generate the full distribution by stripping bad chars, splitting into tokens, lowering then counting
+    phrase = phrase.replace('\r', '').replace('\n', '')
     for char in punc_chars:
         phrase = phrase.replace(char, ' ')
+    normalized_tokens = [token.lower() for token in phrase.split(' ')]
+    unique_tokens = list(set(normalized_tokens))
+    freq_dist = {word:normalized_tokens.count(word) for word in unique_tokens if word != ''}
 
-    tokenized_phrase = phrase.split(' ')
-    normalized_phrase = [token.lower() for token in tokenized_phrase]
-    unique_tokens = list(set(normalized_phrase))
-    return {word:normalized_phrase.count(word) for word in unique_tokens}
+    # now sort so we can get top 10 items in distribution
+    ordered_dist = OrderedDict(sorted(freq_dist.items(), key=lambda x: x[1], reverse=True))
+
+    # trim to top ten. alas, the sort is lost after this, but the worthiness of the items lives on
+    freq_dist = {pair[0]: pair[1] for pair in list(ordered_dist.items())[:10]}
+
+    return freq_dist
